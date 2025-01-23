8618fdbcaef7baa8a5f029a3645786fc82db9a12
Ashu Pednekar <ashupednekar49@gmail.com>
2025-01-23 23:05:33 +0530

    feat: nats pubsub done, added test

--- src/pkg/brokers/mod.rs
+++ src/pkg/brokers/mod.rs
@@ -1,9 +1,11 @@
 use async_trait::async_trait;
 use tokio::sync::broadcast::Sender;
+use crate::prelude::Result;
 
 mod nats;
 mod channels;
 
+#[derive(Debug, Clone)]
 pub struct Message{
     pub subject: String,
     pub data: Vec<u8> 
@@ -11,6 +13,6 @@ pub struct Message{
 
 #[async_trait]
 pub trait Broker{
-    async fn produce(subject: &str, data: Vec<u8>);
-    async fn consume(subject: &str, ch: Sender<Message>);
+    async fn produce(&self, subject: &str, data: Vec<u8>) -> Result<()>;
+    async fn consume(&self, subject: &str, ch: Sender<Message>) -> Result<()>;
 }
--- src/pkg/brokers/nats.rs
+++ src/pkg/brokers/nats.rs
@@ -1,6 +1,7 @@
 use async_nats::Client;
 use async_trait::async_trait;
 use serde::Deserialize;
+use tokio_stream::StreamExt;
 use tokio::sync::broadcast::Sender;
 use crate::prelude::Result;
 use config::{Config, Environment, ConfigError};
@@ -37,11 +38,54 @@ impl NatsPubSub{
 
 #[async_trait]
 impl Broker for NatsPubSub{
-    async fn produce(subject: &str, data: Vec<u8>){
-
+    async fn produce(&self, subject: &str, data: Vec<u8>) -> Result<()>{
+        self.client.publish(subject.to_string(), data.into())
+            .await?; 
+        Ok(())
     }
 
-    async fn consume(subject: &str, ch: Sender<Message>){
+    async fn consume(&self, subject: &str, ch: Sender<Message>) -> Result<()>{
+        let mut subscriber = self.client.subscribe(subject.to_string())
+            .await?;
+        while let Some(msg) = subscriber.next().await{
+            ch.send(Message{
+                subject: subject.to_string(),
+                data: msg.payload.to_vec()
+            })?;
+        };
+        Ok(())
+    }
+}
 
+
+#[cfg(test)]
+mod tests{
+    use tokio::sync::broadcast;
+
+    use super::*;
+
+    #[tokio::test]
+    async fn test_pubsub() -> Result<()>{ 
+        let broker = NatsPubSub::new().await?;
+
+        async fn do_produce<T: Broker>(broker: &T) -> Result<()>{
+            for i in 1..100{
+                broker.produce("foo.bar", format!("sample data number {}", &i).into_bytes()).await?;
+            }
+            Ok(())
+        }
+
+        let (tx, mut rx) = broadcast::channel(16);
+
+        tokio::select! {
+            _ = do_produce(&broker) => {},
+            _ = broker.consume("foo.bar", tx) => {}
+        }
+
+        while let Ok(msg) = rx.recv().await{
+            tracing::debug!("msg: {:?}", &msg)
+        }
+
+        Ok(())
     }
 }
