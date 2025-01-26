c956a061650d84bbc410cd61af8a7e601e6d5c44
Ashu Pednekar <ashupednekar49@gmail.com>
2025-01-26 16:51:26 +0530

    feat: gpt UI added

--- src/pkg/server/serve.rs
+++ src/pkg/server/serve.rs
@@ -1,6 +1,6 @@
 use axum::{routing::get, Router};
 
-use crate::{conf::settings, pkg::server::handlers::{probes::livez, ws::handle_ws}, prelude::Result};
+use crate::{conf::settings, pkg::server::handlers::{probes::livez, ui::handle_ui, ws::handle_ws}, prelude::Result};
 
 
 
@@ -11,7 +11,8 @@ pub async fn listen() -> Result<()> {
             .unwrap();
     let router = Router::new()
         .route("/ws/{from}/{to}/", get(handle_ws))
-        .route("/livez/", get(livez));
+        .route("/livez/", get(livez))
+        .route("/", get(handle_ui));
     tracing::info!("listening at :{}", &settings.listen_port);
     axum::serve(listener, router)
         .await?;
